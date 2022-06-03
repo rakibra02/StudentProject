cf757282999e41f9826592aaed1a1dd6f2f0c966
Rana <105497508+rakibra02@users.noreply.github.com>
2022-06-03 12:54:30 +0400

    Update StudentDetailsPrint.java

--- src/studentdetailsprint/StudentDetailsPrint.java
+++ src/studentdetailsprint/StudentDetailsPrint.java
@@ -32,10 +32,9 @@ public class StudentDetailsPrint {
        for(int i=0;i<list.length;i++)
        {
          System.out.println(list[i].getName() + " "+list[i].getAge());
-        
-        
        }
        //pull operation
+        //fetch and merge
     }
     
 }
